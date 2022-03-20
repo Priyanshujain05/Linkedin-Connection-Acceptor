41676430a6632d4b69657dfcc1a75e001c9793d3
Priyanshu Jain <63446224+Priyanshujain05@users.noreply.github.com>
2022-03-20 13:57:13 +0530

    Add files via upload

---
+++ Linkedin Connection Acceptor/content.js
@@ -0,0 +1,12 @@
+
+
+(function() {
+        //alert("Hello")
+        var connection = document
+        .querySelectorAll(
+        '.invitation-card__action-btn.artdeco-button--secondary');
+        for(var i = 0; i < connection.length; i = i + 1)
+           connection[i].click();
+
+        document.body.appendChild(connection); 
+  })();
